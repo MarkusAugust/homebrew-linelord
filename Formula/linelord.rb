7e4fd7f7965fac35a063aa24f469a7c6aa590c1d
MASK <m.a.sobergklyver@gmail.com>
2025-06-02 00:43:28 +0200

    Bump version to 0.5.6 and update SHA256 checksums for binaries

--- Formula/linelord.rb
+++ Formula/linelord.rb
@@ -1,20 +1,20 @@
 class Linelord < Formula
   desc "Analyze git blame data with developer contribution statistics"
   homepage "https://github.com/MarkusAugust/linelord"
-  version "0.5.5"
+  version "0.5.6"
   
   if OS.linux? && Hardware::CPU.arm?
     url "https://github.com/MarkusAugust/linelord/releases/download/v#{version}/linelord-linux-arm64.tar.gz"
-    sha256 "5f3d501b2cfdb4f5d39ca0df5e44b7fda198a1a7bc11ac37e5444759f4b13a08"
+    sha256 "f10393a3992fdee6214b7dcf5e6741055cc1a4f8ab37f74fb6a09d96c1b7acb1"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/MarkusAugust/linelord/releases/download/v#{version}/linelord-linux-x64.tar.gz"
-    sha256 "713173968e911fb798d967350218f798ff519dd9bfd2744a0697c14408c4ab40"
+    sha256 "a944d363b66320056d50a90c29dcbbaac0dc0681fa60a1315d5e2ecfb746d37e"
   elsif OS.mac? && Hardware::CPU.arm?
     url "https://github.com/MarkusAugust/linelord/releases/download/v#{version}/linelord-macos-arm64.tar.gz"
-    sha256 "9a98050fdde75eeee81c4ec65fa05af85f01e14c0f3292dd357c7e39e4d977bc"
+    sha256 "e5314a789246c0012dd26341ba01f4c13328945dafab15bc2dc14b1671c46077"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/MarkusAugust/linelord/releases/download/v#{version}/linelord-macos-intel.tar.gz"
-    sha256 "bb6ecba3c7fcf787db81b950c8aeb9d3b7ef8e97ca4b64c233f6103979358c10"
+    sha256 "07d4856f9dc724465c660c59906ab9c5447694f26cc806318519e70860a5a887"
   end
 
   def install
